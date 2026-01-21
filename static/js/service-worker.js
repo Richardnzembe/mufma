ffd0e51a641e3f91dcb498832f86c1bb2781b7d7
Richard Nzembe <nzemberichard398@gmail.com>
2026-01-21 17:15:30 +0200

    Add PWA support: service worker + offline fallback

---
+++ static/js/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'mufma-v1';
+const OFFLINE_URL = '/offline/';
+
+const CORE_ASSETS = [
+  '/',
+  '/static/css/style.css',
+  '/static/manifest.json',
+  '/static/icons/ios/180.png',
+  OFFLINE_URL,
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Put a copy in cache
+        const cloned = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(OFFLINE_URL)))
+  );
+});
